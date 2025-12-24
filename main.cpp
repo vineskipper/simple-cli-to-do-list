114e6327be5e0e6dfbc80d058081773f0c493d97
Vineskipper <divine.ezinma@gmail.com>
2025-12-23 22:04:50 -0600

    initial commit

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip> // for std::quoted
+#include <sstream>
+
+// prints the contents of a string stream
+void printSS(std::stringstream& stream){
+    for (std::string line {}; std::getline(stream, line); std::cout << '\n'){
+        std::cout << line;
+    }
+}
+
+// copies the contents of an ifstream to a sstream
+void copy(std::ifstream& input, std::stringstream& output){
+    for (std::string line {}; std::getline(input, line); output << '\n'){
+        output << line;
+    }
+}
+
+int main(int argc, char* argv[]){
+    const std::string filePath {argv[1] ? argv[1] : "config.txt"};
+
+    std::ifstream inputFile {filePath};
+    std::stringstream outputContent {};
+
+    if (inputFile.is_open()){
+        for (std::string line[2] {}; std::getline(inputFile, line[0]); outputContent << '\n'){
+            outputContent << line[0] << ":: " << line[1];
+        }
+
+        printSS(outputContent);
+
+    } else std::cout << std::quoted(filePath) << " was not found.";
+
+    return 0;
+}
